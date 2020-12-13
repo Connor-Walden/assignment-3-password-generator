04a27384afcd03812df258c68d3909d356536fee
Connor Walden <connorwalden1403@gmail.com>
2020-12-13 15:11:51 +1030

    Setup options for password generation and added prompt for password length

--- script.js
+++ script.js
@@ -7,7 +7,48 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var length = 8;
+  var retVal = "";
+
+  var charset = {
+    "option-lowercase": false,
+    "option-uppercase": false,
+    "option-numbers": false,
+    "option-special": false,
+    "lowercase": "abcdefghijklmnopqrstuvwxyz",
+    "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "numbers": "0123456789",
+    "special":  "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
+
+  length = promptLength();
+
+  if(length === null)
+    return;
+
+  promptOptions();
+
+  // for (var i = 0, n = charset.length; i < length; ++i) {
+  //   retVal += charset.charAt(Math.floor(Math.random() * n));
+  // }
+  return retVal;
+}
+
+function promptLength() {
+  var lgth = prompt("How many characters in password?");
+
+  if(lgth <= 128 && lgth >= 8) {
+    return lgth;
+  }
+  else {
+    alert("Password must be between 8 and 128 characters!");
+    generatePassword();  
+  }
+
+  return null;
 }
 
 // Add event listener to generate button
