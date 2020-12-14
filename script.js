4af3b02b179028703a8d74c49c29e84f2fe7be0a
Connor Walden <connorwalden1403@gmail.com>
2020-12-14 17:42:45 +1030

    Moved option prompting code into it's own function

--- script.js
+++ script.js
@@ -36,18 +36,8 @@ function generatePassword() {
   // it must be between 8 and 128 characters and restart the generator.
   length = promptLength();
 
-  // Ask user what options they want for their password.
-  charset["option-lowercase"] = confirm("Would you like the password to include lowercase characters?");
-  charset["option-uppercase"] = confirm("Would you like the password to include uppercase characters?");
-  charset["option-numbers"] = confirm("Would you like the password to include numerical characters?");
-  charset["option-special"] = confirm("Would you like the password to include special characters?");
-
-  // Setting up options for password generation
-  var chars = "";
-  if(charset["option-lowercase"]) chars += charset["lowercase"];
-  if(charset["option-uppercase"]) chars += charset["uppercase"];
-  if(charset["option-numbers"]) chars += charset["numbers"]; 
-  if(charset["option-special"]) chars += charset["special"];
+  // Prompt user for options for password generation
+  var chars = promptCharset(charset);
 
   // If at this point chars still equals nothing, the user set all options
   // to false... This is not allowed so now we alert back that it is isn't allowed
@@ -74,6 +64,23 @@ function promptLength() {
     alert("Password must be between 8 and 128 characters!");
     generatePassword();  
   }
+}
+
+function promptCharset(charset) {
+  // Ask user what options they want for their password.
+  charset["option-lowercase"] = confirm("Would you like the password to include lowercase characters?");
+  charset["option-uppercase"] = confirm("Would you like the password to include uppercase characters?");
+  charset["option-numbers"] = confirm("Would you like the password to include numerical characters?");
+  charset["option-special"] = confirm("Would you like the password to include special characters?");
+
+  // Setting up options for password generation
+  var chars = "";
+  if(charset["option-lowercase"]) chars += charset["lowercase"];
+  if(charset["option-uppercase"]) chars += charset["uppercase"];
+  if(charset["option-numbers"]) chars += charset["numbers"]; 
+  if(charset["option-special"]) chars += charset["special"];
+
+  return chars;
 }
 
 // Add event listener to generate button
